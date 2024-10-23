8a3288de85fe3a35f2ffe934cb0d77646746053a
briansomogyi <brian.somogyi@emanuel.ro>
2024-10-23 07:52:54 +0300

    Successfully updated the database

--- src/ro/emanuel/Main.java
+++ src/ro/emanuel/Main.java
@@ -1,5 +1,7 @@
 package ro.emanuel;
 
+import java.util.List;
+
 import ro.emanuel.pojo.RubiksCube;
 import ro.emanuel.rubickscube.dao.RubiksCubeDAO;
 
@@ -7,8 +9,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
+			// READ
+			List<RubiksCube> rubiksCubes = RubiksCubeDAO.getAll();
+			System.out.println(rubiksCubes);
 			RubiksCube rubiksCube = RubiksCubeDAO.getById(1);
 			System.out.println(rubiksCube);
+			
+			// CREATE
+			int id = 0;
+			String brand = "MoYu";
+			double weight = 3.3;
+			int sides = 6;
+			rubiksCube = new RubiksCube(id, brand, weight, sides);
+			String command = RubiksCubeDAO.create(rubiksCube);
+			System.out.println(command);
+			
+			// READ
+			rubiksCubes = RubiksCubeDAO.getAll();
+			System.out.println(rubiksCubes);
+			
+			// UPDATE
+			id = 1;
+			rubiksCube = new RubiksCube(id, brand, weight, sides);
+			command = RubiksCubeDAO.update(rubiksCube);
+			System.out.println(command);
+			
+			// DELETE
+			command = RubiksCubeDAO.delete(id);
+			System.out.println(command);
+						
+			// READ
+			rubiksCubes = RubiksCubeDAO.getAll();
+			System.out.println(rubiksCubes);
+									
 		} catch (Exception exception) {
 			System.out.println(exception);
 		}
--- src/ro/emanuel/rubickscube/dao/RubiksCubeDAO.java
+++ src/ro/emanuel/rubickscube/dao/RubiksCubeDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
 import ro.emanuel.DataBaseHelper;
 import ro.emanuel.pojo.RubiksCube;
@@ -13,7 +14,7 @@ import ro.emanuel.pojo.RubiksCube;
 public class RubiksCubeDAO {
 	public static RubiksCube getById(int id) throws SQLException {
 		Connection connection = DataBaseHelper.getConnection();
-		String query = "SELECT * FROM rubiks_cube where id=?";
+		String query = "select * from rubiks_cube where id=?";
 		PreparedStatement getRubiksCube = connection.prepareStatement(query);
 		getRubiksCube.setInt(1, id);
 
@@ -22,14 +23,12 @@ public class RubiksCubeDAO {
 		if (resultSet.next())
 			rubiksCube = new RubiksCube(resultSet.getInt("id"), resultSet.getString("brand"), resultSet.getDouble("weight"), resultSet.getInt("sides"));
 		DataBaseHelper.closeConnection();
-		if (rubiksCube != null)
-			return rubiksCube;
-		return null;
+		return rubiksCube;
 	}
 
-	public static ArrayList<RubiksCube> getAll() throws SQLException {
+	public static List<RubiksCube> getAll() throws SQLException {
 		Connection connection = DataBaseHelper.getConnection();
-		String query = "SELECT * FROM rubiks_cube";
+		String query = "select * from rubiks_cube";
 		Statement statement = connection.createStatement();
 
 		ResultSet resultSet = statement.executeQuery(query);
@@ -43,5 +42,48 @@ public class RubiksCubeDAO {
 		}
 		DataBaseHelper.closeConnection();
 		return rubiksCubes;
+	}
+	
+	public static String create (RubiksCube rubiksCube) throws SQLException {
+		Connection connection = DataBaseHelper.getConnection();
+		
+		String query = "insert into rubiks_cube (brand, weight, sides) values (?, ?, ?)";
+		PreparedStatement createRubiksCube = connection.prepareStatement(query);
+		
+		createRubiksCube.setString(1, rubiksCube.getBrand());
+		createRubiksCube.setDouble(2, rubiksCube.getWeight());
+		createRubiksCube.setInt(3, rubiksCube.getSides());
+		int response = createRubiksCube.executeUpdate();
+		DataBaseHelper.closeConnection();
+		return response + " rows affected.";
+	}
+	
+	public static String delete(int id) throws SQLException {
+		Connection connection = DataBaseHelper.getConnection();
+		
+		String query = "delete from rubiks_cube where id>?";
+		PreparedStatement deleteRubiksCube = connection.prepareStatement(query);
+		
+		deleteRubiksCube.setInt(1, id);
+		
+		int response = deleteRubiksCube.executeUpdate();
+		DataBaseHelper.closeConnection();
+		return response + " rows affected.";
+	}
+	
+	public static String update(RubiksCube rubiksCube) throws SQLException {
+		Connection connection = DataBaseHelper.getConnection();
+		
+		String query = "update rubiks_cube set brand=?, weight=?, sides=? where id=?";
+		PreparedStatement updateRubiksCube = connection.prepareStatement(query);
+		
+		updateRubiksCube.setString(1, rubiksCube.getBrand());
+		updateRubiksCube.setDouble(2, rubiksCube.getWeight());
+		updateRubiksCube.setInt(3, rubiksCube.getSides());
+		updateRubiksCube.setInt(4, rubiksCube.getId());
+		
+		int response = updateRubiksCube.executeUpdate();
+		DataBaseHelper.closeConnection();
+		return response + " rows affected.";
 	}
 }
